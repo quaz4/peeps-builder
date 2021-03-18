269fb5d6d49be601c1781110824dc04f752eee28
William Stewart <hmaswill@gmail.com>
2021-03-18 09:46:37 +0800

    Angry Face

--- src/head/face/FaceFactory.ts
+++ src/head/face/FaceFactory.ts
@@ -1,9 +1,12 @@
+import { Angry } from "./Angry";
 import { Cheeky } from "./Cheeky";
 import { Face } from "./Face";
 
 export class FaceFactory {
     static buildFace(face: string): Face | null {
         switch (face) {
+            case "Angry":
+                return new Angry();
             case "Cheeky":
                 return new Cheeky();
             default:
