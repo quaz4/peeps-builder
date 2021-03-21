b783bf1fc3f80bc521ac10b6d27134d158b67aae
William Stewart <hmaswill@gmail.com>
2021-03-21 15:49:43 +0800

    Make test generator write to a file

--- test/Test.ts
+++ test/Test.ts
@@ -2,7 +2,7 @@ import { Cheeky } from "../src/head/face/Cheeky";
 import { Peep } from "../src/Peep";
 import { PeepConfig } from "../src/PeepConfig";
 import { Device } from "../src/pose/bust/Device";
-
+import fs from 'fs';
 // console.log(new Peep(-350, -150, 1500, 1500, new Device(new Cheeky())).build());
 
 const peep: PeepConfig = {
@@ -19,4 +19,6 @@ const peep: PeepConfig = {
     }
 }
 
-console.log(new Peep(peep).build());
+fs.writeFile('test/test.svg', new Peep(peep).build(), "utf8", () => {
+    console.log("Written to test.svg");
+});
