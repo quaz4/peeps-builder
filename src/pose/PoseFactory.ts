c7147a9ca0d3670c18474ead5c325657b0fbf1a2
William Stewart <hmaswill@gmail.com>
2021-03-28 14:32:06 +0800

    Added BlazerBlackTee

--- src/pose/PoseFactory.ts
+++ src/pose/PoseFactory.ts
@@ -1,3 +1,4 @@
+import { BlazerBlackTee } from "./bust/BlazerBlackTee";
 import { Device } from "./bust/Device";
 import { Pose } from "./Pose";
 import { PoseConfig } from "./PoseConfig";
@@ -5,6 +6,8 @@ import { PoseConfig } from "./PoseConfig";
 export class PoseFactory {
     static buildPose(config: PoseConfig): Pose | null {
         switch (config.name) {
+            case "BlazerBlackTee":
+                return new BlazerBlackTee(config.strokeColor, config.fillColor);
             case "Device":
                 return new Device(config.strokeColor, config.fillColor);
             default:
